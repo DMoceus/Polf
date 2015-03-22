00119ecfce94d4253cb90ddc15adce787c3bb15b
daveinthecave <daveman0331@gmail.com>
2015-03-21 23:43:08 -0400

    Trying to move Overlay

---
+++ src/aimshot.h
@@ -0,0 +1,5 @@
+#include <pebble.h>
+
+void aim_shot_course_layer_update_callback(Layer* layer, GContext* ctx);
+void aim_shot_window_load(Window* window);
+void aim_shot_window_unload(Window* window);
--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include "about.h"
 #include "showcourse.h"
 #include "pickclub.h"
+#include "calculations.h"
+#include "aimshot.h"
 
 Window* main_menu_window;
 MenuLayer* main_menu_layer;
@@ -12,20 +14,133 @@ TextLayer* about_layer;
 
 Window* show_course_window;
 Layer* show_course_layer;
-GPath* hole_one;
 
 Window* pick_club_window;
 MenuLayer* pick_club_layer;
+
+Window* aim_shot_window;
+Layer* aim_shot_course_layer;
+RotBitmapLayer* aim_shot_overlay_layer;
+GBitmap* bitMap;
+
 int club_choice;
 int hit_low_range;
 int hit_high_range;
+int inTheRough;
 
+int globalAngle;
+int quadrant;
+int actualAngle;
+
+
+
+//Calculations
+
+void initializeAngle(){
+    globalAngle = 0;
+    quadrant = 2;
+    actualAngle = 90;
+}
+
+void rotateRight(){
+    globalAngle += 6;
+    if(360 <= globalAngle){
+        globalAngle = globalAngle - 360;
+    }
+    if(0 <= globalAngle && globalAngle <= 90){
+        quadrant = 2;
+        actualAngle = 90 - globalAngle;
+    } else if(90 < globalAngle && globalAngle <= 180){
+        quadrant = 4;
+        actualAngle = globalAngle - 90;
+    } else if(180 < globalAngle && globalAngle <= 270){
+        quadrant = 3;
+        actualAngle = 270 - globalAngle;
+    } else{
+        quadrant = 1;
+        actualAngle = globalAngle - 270;
+    }
+}
+void rotateLeft(){
+    if(globalAngle < 6){
+        globalAngle = globalAngle + 360;
+    }
+    globalAngle = globalAngle - 6;
+    if(0 <= globalAngle && globalAngle <= 90){
+        quadrant = 2;
+        actualAngle = 90 - globalAngle;
+    } else if(90 < globalAngle && globalAngle <= 180){
+        quadrant = 4;
+        actualAngle = globalAngle - 90;
+    } else if(180 < globalAngle && globalAngle <= 270){
+        quadrant = 3;
+        actualAngle = 270 - globalAngle;
+    } else{
+        quadrant = 1;
+        actualAngle = globalAngle - 270;
+    }
+}
+
+int roll_power(){
+    //praise be to RNGesus
+    int delta = hit_high_range - hit_low_range;
+    srand(time(NULL));
+    int show_me_7 = (rand() % delta)+hit_low_range;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "%d", show_me_7);
+    return show_me_7;
+}
+int calcYCoord(int angleIn, int power){
+    angleIn = angleIn%360;
+    int angle = (angleIn*TRIG_MAX_ANGLE)/360;
+    int32_t ratio= ((sin_lookup(angle))*1000)/TRIG_MAX_RATIO;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "%ld", ((ratio*power)+500)/1000);
+    return ((ratio*5)+500)/1000;
+}
+int calcXCoord(int angleIn, int power){
+    angleIn = angleIn%360;
+    int angle = (angleIn*TRIG_MAX_ANGLE)/360;
+    int32_t ratio= ((cos_lookup(angle))*1000)/TRIG_MAX_RATIO;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "%ld", ((ratio*power)+500)/1000);
+    return ((ratio*power)+500)/1000;
+}
+
+//Aim Shot Callbacks
+
+void aim_shot_course_layer_update_callback(Layer* layer, GContext* ctx){
+    graphics_context_set_fill_color(ctx, GColorJaegerGreen);
+    gpath_draw_filled(ctx, hole_one);
+    graphics_context_set_fill_color(ctx, GColorDarkGreen);
+    gpath_draw_filled(ctx, green_path);
+    graphics_context_set_fill_color(ctx, GColorWhite);
+    gpath_draw_filled(ctx, hole_path);
+    graphics_context_set_fill_color(ctx, GColorChromeYellow);
+    gpath_draw_filled(ctx, sand1_path);
+    graphics_context_set_fill_color(ctx, GColorChromeYellow);
+    gpath_draw_filled(ctx, sand2_path);
+    graphics_context_set_fill_color(ctx, GColorChromeYellow);
+    gpath_draw_filled(ctx, sand3_path);
+    graphics_context_set_fill_color(ctx, GColorDarkGray);
+    gpath_draw_filled(ctx, teebox_path);
+    rot_bitmap_set_compositing_mode(aim_shot_overlay_layer, GCompOpSet);
+}
 
 //Show Course Callbacks
 
 void show_course_layer_update_callback(Layer* layer, GContext* ctx){
     graphics_context_set_fill_color(ctx,GColorJaegerGreen);
     gpath_draw_filled(ctx,hole_one);
+    graphics_context_set_fill_color(ctx,GColorDarkGreen);
+    gpath_draw_filled(ctx,green_path);
+    graphics_context_set_fill_color(ctx,GColorWhite);
+    gpath_draw_filled(ctx,hole_path);
+    graphics_context_set_fill_color(ctx,GColorChromeYellow);
+    gpath_draw_filled(ctx,sand1_path);
+    graphics_context_set_fill_color(ctx,GColorChromeYellow);
+    gpath_draw_filled(ctx,sand2_path);
+    graphics_context_set_fill_color(ctx,GColorChromeYellow);
+    gpath_draw_filled(ctx,sand3_path);
+    graphics_context_set_fill_color(ctx,GColorDarkGray);
+    gpath_draw_filled(ctx,teebox_path);
 }
 
 //Pick Club Callbacks
@@ -49,39 +164,52 @@ void pc_draw_row_callback(GContext* ctx, Layer* cell_layer, MenuIndex* cell_inde
             break;
     }
 }
+
 uint16_t pc_num_rows_callback(MenuLayer* menu_layer, uint16_t section_index, void* callback_context){
     return 5;
 }
 
 void pc_select_click_callback(MenuLayer* menu_layer, MenuIndex* cell_index, void* callback_context){
-        //TODO: Link To Next State Once Next State is Made
     switch(cell_index->row){
         case 0:
             club_choice = PUTTER;
             hit_low_range = PUTTER_LOW;
             hit_high_range = PUTTER_HIGH;
+            bitMap = gbitmap_create_with_resource(RESOURCE_ID_OVERLAY_PUTTER);
             break;
         case 1:
             club_choice = IRON_NINE;
             hit_low_range = IRON_NINE_LOW;
             hit_high_range = IRON_NINE_HIGH;
+            bitMap = gbitmap_create_with_resource(RESOURCE_ID_OVERLAY_IRON_NINE);
             break;
         case 2:
             club_choice = IRON_SEVEN;
             hit_low_range = IRON_SEVEN_LOW;
             hit_high_range = IRON_SEVEN_HIGH;
+            bitMap = gbitmap_create_with_resource(RESOURCE_ID_OVERLAY_IRON_SEVEN);
             break;
         case 3:
             club_choice = IRON_FIVE;
             hit_low_range = IRON_FIVE_LOW;
             hit_high_range = IRON_FIVE_HIGH;
+            bitMap = gbitmap_create_with_resource(RESOURCE_ID_OVERLAY_IRON_FIVE);
             break;
         case 4:
             club_choice = IRON_THREE;
             hit_low_range = IRON_THREE_LOW;
             hit_high_range = IRON_THREE_HIGH;
+            bitMap = gbitmap_create_with_resource(RESOURCE_ID_OVERLAY_IRON_THREE);
             break;
     }
+    aim_shot_window = window_create();
+    aim_shot_overlay_layer = rot_bitmap_layer_create(bitMap);
+    window_set_background_color(aim_shot_window, GColorArmyGreen);
+    window_set_window_handlers(aim_shot_window, (WindowHandlers){
+        .load   = aim_shot_window_load,
+        .unload = aim_shot_window_unload
+    });
+    window_stack_push(aim_shot_window,true);
 }
 
 //Main Menu Callbacks
@@ -107,6 +235,12 @@ void mm_select_click_callback(MenuLayer* menu_layer, MenuIndex* cell_index, void
         case 0:
             show_course_window = window_create();
             hole_one = gpath_create(&HOLE_ONE_FAIRWAY);
+            green_path = gpath_create(&GREEN);
+            hole_path = gpath_create(&HOLE);
+            sand1_path = gpath_create(&SAND1);
+            sand2_path = gpath_create(&SAND2);
+            sand3_path = gpath_create(&SAND3);
+            teebox_path = gpath_create(&TEEBOX);
             window_set_background_color(show_course_window, GColorBlack);
             window_set_window_handlers(show_course_window, (WindowHandlers){
                 .load   = show_course_window_load,
@@ -136,15 +270,29 @@ void show_course_select_click_handler(ClickRecognizerRef recognizer, void* conte
     };
     window_set_window_handlers(pick_club_window,(WindowHandlers)pc_handlers);
     window_stack_push(pick_club_window,true);
-    //static const uint32_t const segments[] = {200,100,400};
 }
 
 void show_course_config_provider(Window* window){
     window_single_click_subscribe(BUTTON_ID_SELECT, show_course_select_click_handler);
-    //window_single_click_subscribe
 }
 
 //LOADERS
+
+void aim_shot_window_load(Window* window){
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Aim Shot Window Loading");
+    aim_shot_course_layer = layer_create(GRect(0,0,144,168-MENU_CELL_BASIC_HEADER_HEIGHT));
+    layer_set_update_proc(aim_shot_course_layer,aim_shot_course_layer_update_callback);
+    layer_add_child(window_get_root_layer(aim_shot_window), aim_shot_course_layer);
+    //TODO ATTACH BITMAP LAYER
+    layer_add_child(window_get_root_layer(aim_shot_window),bitmap_layer_get_layer((BitmapLayer*)aim_shot_overlay_layer));
+    initializeAngle();
+    
+}
+
+void aim_shot_window_unload(Window* window){
+    rot_bitmap_layer_destroy(aim_shot_overlay_layer);
+    layer_destroy(aim_shot_course_layer);
+}
 
 void pick_club_window_load(Window* window){
     pick_club_layer = menu_layer_create(GRect(0,0,144,168-MENU_CELL_BASIC_HEADER_HEIGHT));
@@ -171,6 +319,12 @@ void show_course_window_load(Window* window){
 
 void show_course_window_unload(Window* window){
     gpath_destroy(hole_one);
+    gpath_destroy(green_path);
+    gpath_destroy(hole_path);
+    gpath_destroy(sand1_path);
+    gpath_destroy(sand2_path);
+    gpath_destroy(sand3_path);
+    gpath_destroy(teebox_path);
     layer_destroy(show_course_layer);
 }
 
